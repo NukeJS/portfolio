41d9a639913acc5a69b2479cfded9d2e690048e3
ronniew <rwaebers@gmail.com>
2021-06-17 15:02:45 +0200

    Added favicons and metatags

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,15 +8,23 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'I\'m a guy that is fond of creating full stack web apps.I\'m currently based in the Netherlands. I create modern and functional web apps.' }
+      { hid: 'description', name: 'description', content: 'I\'m a guy that is fond of creating full stack web apps.I\'m currently based in the Netherlands. I create modern and functional web apps.' },
+      { name: 'msapplication-TileColor', content: '#ffffff' },
+      { name: 'msapplication-config', content: '/favicons/browserconfig.xml' },
+      { name: 'theme-color', content: '#ffffff' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'apple-touch-icon', sizes: '180x180', href: '/favicons/apple-touch-icon.png' },
+      { rel: 'icon', type: 'image/png', href: '/favicons/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', href: '/favicons/favicon-16x16.png' },
+      { rel: 'manifest', href: '/favicons/site.webmanifest' },
+      { rel: 'mask-icon', href: '/favicons/safari-pinned-tab.svg', color: '#6366f1' },
+      { rel: 'shortcut icon', href: '/favicons/favicon.ico' },
 
       // Font Family
       { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap' },
-    ]
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
