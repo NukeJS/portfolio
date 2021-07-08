26437bcd5cac65eab75d15eb41dd622325e6793a
ronniew <rwaebers@gmail.com>
2021-07-08 19:39:48 +0200

    Fix

--- components/layouts/shared/Footer.vue
+++ components/layouts/shared/Footer.vue
@@ -2,8 +2,8 @@
   <rc-footer class="border-t bg-white dark:bg-gray-900 dark:border-gray-700">
     <rc-container class="flex items-center justify-center">
       <p class="text-sm text-gray-700 dark:text-gray-400 font-semibold">
-        Copyright {{ new Date().getFullYear() }} - RonnieCodes. All rights
-        reserved.
+        Copyright {{ new Date().getFullYear() }} - RonnieCodes.
+        <span class="hidden sm:inline">All rights reserved.</span>
       </p>
     </rc-container>
   </rc-footer>
