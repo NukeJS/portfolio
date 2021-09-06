03c4d9f4d2223832c8cda3c88f7b3b4eef5dc5e1
ronniew <rwaebers@gmail.com>
2021-09-06 18:26:11 +0200

    test

--- components/global/base/CodeGroup.vue
+++ components/global/base/CodeGroup.vue
@@ -56,6 +56,7 @@ export default {
       this.tabs[i].elm.classList.add("code-block--active");
     },
     updateTabs(i) {
+      console.log(i);
       this.activeTabIndex = i;
       this.updateHighlighteUnderlinePosition();
     },
