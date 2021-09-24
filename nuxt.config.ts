afb4d0b4b5c7e6a07602b804cdafcb575be3dffc
ronniew <rwaebers@gmail.com>
2021-09-24 08:55:18 +0200

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,90 +5,81 @@ const config: NuxtConfig = {
   target: "static",
 
   // Global page headers: https://go.nuxtjs.dev/config-head
-  head() {
-    const i18nHead = this.$nuxtI18nHead({ addSeoAttributes: true });
-
-    return {
-      titleTemplate: "%s - RonnieCodes",
-      title: "RonnieCodes",
-      htmlAttrs: {
-        ...i18nHead.htmlAttrs
+  head: {
+    titleTemplate: "%s - RonnieCodes",
+    title: "RonnieCodes",
+    htmlAttrs: {
+      lang: "en"
+    },
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content:
+          "I'm a guy that is fond of creating full stack web apps.I'm currently based in the Netherlands. I create modern and functional web apps."
       },
-      meta: [
-        { charset: "utf-8" },
-        { name: "viewport", content: "width=device-width, initial-scale=1" },
-        {
-          hid: "description",
-          name: "description",
-          content:
-            "I'm a guy that is fond of creating full stack web apps.I'm currently based in the Netherlands. I create modern and functional web apps."
-        },
-        {
-          hid: "keywords",
-          name: "keywords",
-          content:
-            "Web Development, Full Stack, Ronnie, RonnieCodes, Web Developer, Node.js, Vue.js, Nuxt.js, Node.js Developer"
-        },
+      {
+        hid: "keywords",
+        name: "keywords",
+        content:
+          "Web Development, Full Stack, Ronnie, RonnieCodes, Web Developer, Node.js, Vue.js, Nuxt.js, Node.js Developer"
+      },
 
-        {
-          hid: "og:url",
-          property: "og:url",
-          content: "https://ronniecodes.com"
-        },
-        {
-          hid: "og:description",
-          property: "og:description",
-          content:
-            "I'm a guy that is fond of creating full stack web apps.I'm currently based in the Netherlands. I create modern and functional web apps."
-        },
+      {
+        hid: "og:url",
+        property: "og:url",
+        content: "https://ronniecodes.com"
+      },
+      {
+        hid: "og:description",
+        property: "og:description",
+        content:
+          "I'm a guy that is fond of creating full stack web apps.I'm currently based in the Netherlands. I create modern and functional web apps."
+      },
 
-        { name: "twitter:site", content: "@TheCodingNuke" },
-        { name: "twitter:card", content: "summary_large_image" },
-        {
-          hid: "twitter:url",
-          name: "twitter:url",
-          content: "https://ronniecodes.com"
-        },
-        {
-          hid: "twitter:description",
-          name: "twitter:description",
-          content:
-            "I'm a guy that is fond of creating full stack web apps.I'm currently based in the Netherlands. I create modern and functional web apps."
-        },
+      { name: "twitter:site", content: "@TheCodingNuke" },
+      { name: "twitter:card", content: "summary_large_image" },
+      {
+        hid: "twitter:url",
+        name: "twitter:url",
+        content: "https://ronniecodes.com"
+      },
+      {
+        hid: "twitter:description",
+        name: "twitter:description",
+        content:
+          "I'm a guy that is fond of creating full stack web apps.I'm currently based in the Netherlands. I create modern and functional web apps."
+      },
 
-        { name: "msapplication-TileColor", content: "#ffffff" },
-        {
-          name: "msapplication-config",
-          content: "/favicons/browserconfig.xml"
-        },
-        { name: "theme-color", content: "#ffffff" },
-        ...i18nHead.meta
-      ],
-      link: [
-        {
-          rel: "apple-touch-icon",
-          sizes: "180x180",
-          href: "/favicons/apple-touch-icon.png"
-        },
-        { rel: "icon", type: "image/png", href: "/favicons/favicon-32x32.png" },
-        { rel: "icon", type: "image/png", href: "/favicons/favicon-16x16.png" },
-        {
-          rel: "mask-icon",
-          href: "/favicons/safari-pinned-tab.svg",
-          color: "#6366f1"
-        },
-        { rel: "shortcut icon", href: "/favicons/favicon.ico" },
+      { name: "msapplication-TileColor", content: "#ffffff" },
+      { name: "msapplication-config", content: "/favicons/browserconfig.xml" },
+      { name: "theme-color", content: "#ffffff" }
+    ],
+    link: [
+      {
+        rel: "apple-touch-icon",
+        sizes: "180x180",
+        href: "/favicons/apple-touch-icon.png"
+      },
+      { rel: "icon", type: "image/png", href: "/favicons/favicon-32x32.png" },
+      { rel: "icon", type: "image/png", href: "/favicons/favicon-16x16.png" },
+      {
+        rel: "mask-icon",
+        href: "/favicons/safari-pinned-tab.svg",
+        color: "#6366f1"
+      },
+      { rel: "shortcut icon", href: "/favicons/favicon.ico" },
 
-        // Font Family
-        { rel: "preconnect", href: "https://fonts.gstatic.com" },
-        {
-          rel: "stylesheet",
-          href:
-            "https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap"
-        },
-        ...i18nHead.link
-      ]
-    };
+      // Font Family
+      { rel: "preconnect", href: "https://fonts.gstatic.com" },
+      {
+        rel: "stylesheet",
+        href:
+          "https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap"
+      }
+    ]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
