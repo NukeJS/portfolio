3d2784abecc4c6dd1c947abc118aabc2a5b3eeae
ronniew <rwaebers@gmail.com>
2022-05-25 23:04:35 +0200

    some placeholder text

--- server/api/jobs.ts
+++ server/api/jobs.ts
@@ -7,12 +7,15 @@ export default defineEventHandler((event) => {
         {
           label: "Full-time",
           title: "Full-stack Developer",
-          timespan: "Jul. 2022 - present",
+          timespan: "Jun. 2022 - present",
+          description: "During my time at Script I worked on several projects.",
         },
         {
           label: "Internship",
           title: "Full-stack Developer",
-          timespan: "Feb. 2022 - Jul. 2022",
+          timespan: "Feb. 2022 - Jun. 2022",
+          description:
+            "During my internship at Script I worked on a narrowcasting system using Vue 3 and Laravel.",
         },
       ],
     },
@@ -24,6 +27,8 @@ export default defineEventHandler((event) => {
           label: "Internship",
           title: "Front-end Developer",
           timespan: "Sep. 2020 - Feb. 2021",
+          description:
+            "During my internship at Prophyta I worked on an e-commerce system using Nuxt 2 and Vuetify.",
         },
       ],
     },
