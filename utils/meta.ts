0ceb8a29264f116b76a739940b98b1a6d2d94619
ronniew <rwaebers@gmail.com>
2022-07-10 21:35:34 +0200

    fix meta

--- utils/meta.ts
+++ utils/meta.ts
@@ -30,11 +30,13 @@ export const useHeadHelper = ({
     title,
     meta: [
       /* ------------------------------- Open Graph ------------------------------- */
+      { hid: 'og:title', property: 'og:title', content: title },
       { hid: 'og:type', property: 'og:type', content: type },
       { hid: 'og:url', property: 'og:url', content: url },
       /* -------------------------------------------------------------------------- */
 
       /* --------------------------------- Twitter -------------------------------- */
+      { hid: 'twitter:title', name: 'twitter:title', content: title },
       { hid: 'twitter:url', name: 'twitter:url', content: url },
       {
         hid: 'twitter:creator',
