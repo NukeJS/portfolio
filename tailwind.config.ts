3c1edf7bf6e9c8a7dbdd98c82577ec88b0f98440
NukeJS <rwaebers@gmail.com>
2024-02-14 15:20:25 +0100

    remove

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,9 +13,6 @@ export default <Config>{
   ],
   theme: {
     extend: {
-      screens: {
-        '-md': { raw: '(width < 768px)' },
-      },
       fontFamily: {
         'work-sans': ["'Work Sans'", 'sans-serif'],
         montserrat: ['Montserrat', 'sans-serif'],
